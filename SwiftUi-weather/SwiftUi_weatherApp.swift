59c87f19055f029df77cd6ef11563f38e9d90275
jasim0021 <skjasimuddin9064@gmail.com>
2024-08-13 06:58:55 +0530

    Initial Commit

---
+++ SwiftUi-weather/SwiftUi_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_weatherApp.swift
+//  SwiftUi-weather
+//
+//  Created by Sk Jasimuddin on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
